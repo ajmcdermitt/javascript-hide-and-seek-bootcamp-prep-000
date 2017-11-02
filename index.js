036e2e0ea3ece7852d82a23a95d734342f453428
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-02 17:29:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector){
+   return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n){
+  var lis = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var lis = document.querySelector(div#grand-node);
+
+
+}
